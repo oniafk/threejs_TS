9b22589a5890d9f9a58915fe887cd0274f7176c4
oniafk <manufelipearias@gmail.com>
2025-03-10 18:07:32 +1100

    gltf animation

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,22 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import Stats from "three/addons/libs/stats.module.js";
-import JEASINGS from "jeasings";
 
 const scene = new THREE.Scene();
 
-const gridHelper = new THREE.GridHelper();
-gridHelper.position.y = -1;
+const gridHelper = new THREE.GridHelper(100, 100);
 scene.add(gridHelper);
 
 const hdr = "https://sbcode.net/img/venice_sunset_1k.hdr";
 
-await new RGBELoader().loadAsync(hdr).then((texture) => {
+new RGBELoader().load(hdr, (texture) => {
   texture.mapping = THREE.EquirectangularReflectionMapping;
   scene.environment = texture;
+  scene.background = texture;
+  scene.backgroundBlurriness = 1;
 });
 
 const camera = new THREE.PerspectiveCamera(
@@ -25,12 +25,9 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(0, 1, 4);
+camera.position.set(3, 2, 5);
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 0.8;
-renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -38,122 +35,110 @@ window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  //render() //this line is unnecessary if you are re-rendering within the animation loop
 });
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-//controls.addEventListener('change', render) //this line is unnecessary if you are re-rendering within the animation loop
-
-let suzanne: THREE.Mesh, plane: THREE.Mesh;
-
-new GLTFLoader().load("models/suzanne_scene.glb", (gltf) => {
-  suzanne = gltf.scene.getObjectByName("Suzanne") as THREE.Mesh;
-  suzanne.castShadow = true;
-
-  plane = gltf.scene.getObjectByName("Plane") as THREE.Mesh;
-  plane.scale.set(50, 1, 50);
-  (plane.material as THREE.MeshStandardMaterial).envMap = scene.environment; // since three@163, we need to set `envMap` before changing `envMapIntensity` has any effect.
-  (plane.material as THREE.MeshStandardMaterial).envMapIntensity = 0.05;
-  plane.receiveShadow = true;
-
-  const spotLight = gltf.scene.getObjectByName("Spot") as THREE.SpotLight;
-  spotLight.intensity /= 500;
-  spotLight.castShadow = true;
-  spotLight.target = suzanne;
-
-  scene.add(gltf.scene);
-
-  //render()
-});
-
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-
-renderer.domElement.addEventListener("dblclick", (e) => {
-  mouse.set(
-    (e.clientX / renderer.domElement.clientWidth) * 2 - 1,
-    -(e.clientY / renderer.domElement.clientHeight) * 2 + 1
-  );
-
-  raycaster.setFromCamera(mouse, camera);
-
-  const intersects = raycaster.intersectObjects([plane], false);
-
-  if (intersects.length) {
-    const p = intersects[0].point;
-
-    // controls.target.set(p.x, p.y, p.z);
-
-    // // JEasing the controls.target
-    // new JEASINGS.JEasing(controls.target)
-    //   .to(
-    //     {
-    //       x: p.x,
-    //       y: p.y,
-    //       z: p.z,
-    //     },
-    //     800
-    //   )
-    //   //   .delay(1000)
-    //   .easing(JEASINGS.Cubic.Out)
-    //   //.onUpdate(() => render())
-    //   .start();
-
-    // slding x,z
-    new JEASINGS.JEasing(suzanne.position)
-      .to(
-        {
-          x: p.x,
-          z: p.z,
-        },
-        500
-      )
-      .start();
-
-    // going up
-    new JEASINGS.JEasing(suzanne.position)
-      .to(
-        {
-          y: p.y + 3,
-        },
-        250
-      )
-      .easing(JEASINGS.Cubic.Out)
-      .start()
-      .onComplete(() => {
-        // going down
-        new JEASINGS.JEasing(suzanne.position)
-          .to(
-            {
-              y: p.y + 1,
-            },
-            500
-          )
-          //.delay(500)
-          .easing(JEASINGS.Bounce.Out)
-          .start();
-      });
-  }
-});
+controls.target.set(0, 0.75, 0);
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
 
+function lerp(from: number, to: number, speed: number) {
+  const amount = (1 - speed) * from + speed * to;
+  return Math.abs(from - to) < 0.001 ? to : amount;
+}
+
+let mixer: THREE.AnimationMixer;
+let animationActions: { [key: string]: THREE.AnimationAction } = {};
+let activeAction: THREE.AnimationAction;
+let speed = 0,
+  toSpeed = 0;
+
+// new GLTFLoader().load("models/queen$@thinking.glb", (gltf) => {
+//   mixer = new THREE.AnimationMixer(gltf.scene);
+//   //   console.log(gltf);
+
+//   mixer.clipAction(gltf.animations[0]).play();
+
+//   scene.add(gltf.scene);
+// });
+
+async function loadEve() {
+  const loader = new GLTFLoader();
+  const [eve, yelling, run] = await Promise.all([
+    loader.loadAsync("models/queen$@thinking.glb"),
+    loader.loadAsync("models/queen@yelling.glb"),
+    loader.loadAsync("models/queen@run.glb"),
+  ]);
+
+  mixer = new THREE.AnimationMixer(eve.scene);
+
+  // mixer.clipAction(yelling.animations[0]).play();
+
+  animationActions["yelling"] = mixer.clipAction(yelling.animations[0]);
+  animationActions["thinking"] = mixer.clipAction(eve.animations[0]);
+  animationActions["run"] = mixer.clipAction(run.animations[0]);
+
+  animationActions["thinking"].play();
+  activeAction = animationActions["thinking"];
+
+  scene.add(eve.scene);
+}
+await loadEve();
+
+const keyMap: { [key: string]: boolean } = {};
+
+const onDocumentKey = (e: KeyboardEvent) => {
+  keyMap[e.code] = e.type === "keydown";
+};
+document.addEventListener("keydown", onDocumentKey, false);
+document.addEventListener("keyup", onDocumentKey, false);
+
+const clock = new THREE.Clock();
+let delta = 0;
+
 function animate() {
   requestAnimationFrame(animate);
 
+  delta = clock.getDelta();
+
   controls.update();
 
-  JEASINGS.update();
+  mixer.update(delta);
 
-  render();
+  if (keyMap["KeyR"]) {
+    //walk
+    if (activeAction != animationActions["run"]) {
+      activeAction.fadeOut(1);
+      animationActions["run"].reset().fadeIn(0.5).play();
+      activeAction = animationActions["run"];
+      toSpeed = 20;
+    }
+  } else if (keyMap["KeyY"]) {
+    if (activeAction != animationActions["yelling"]) {
+      activeAction.fadeOut(0.5);
+      animationActions["yelling"].reset().fadeIn(0.25).play();
+      activeAction = animationActions["yelling"];
+      toSpeed = 0;
+    }
+  } else {
+    //idle
+    if (activeAction != animationActions["thinking"]) {
+      activeAction.fadeOut(0.5);
+      animationActions["thinking"].reset().fadeIn(0.25).play();
+      activeAction = animationActions["thinking"];
+      toSpeed = 0;
+    }
+  }
+
+  speed = lerp(speed, toSpeed, delta * 10);
+  gridHelper.position.z -= speed * delta;
+  gridHelper.position.z = gridHelper.position.z % 10;
+
+  renderer.render(scene, camera);
 
   stats.update();
-}
-
-function render() {
-  renderer.render(scene, camera);
 }
 
 animate();
